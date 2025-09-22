728374e41fd5023c03f0a8b02edd4b8fbb49b3e4
shakiroorakam <shakirokm@gmail.com>
2025-09-22 15:50:08 +0530

    admin router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+// This reads the "homepage" field from your package.json
+const basename = process.env.PUBLIC_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Wrap the App component with the router */}
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
